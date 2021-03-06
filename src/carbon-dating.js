9241c3385448405e63d075acfa6115697beaa4e9
kykysja <kykysja11@gmail.com>
2021-03-06 19:10:09 +0100

    fix: fix implementation of carbon-dating regarding to the new tests

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,11 +6,11 @@ const HALF_LIFE_PERIOD = 5730;
 module.exports = function dateSample(sampleActivity) {
 	let t;
 
-	if (typeof sampleActivity != 'string' | sampleActivity > 15) {
+	if (typeof sampleActivity != 'string' || sampleActivity > 15) {
 		return false;
 	} else {
 		t = Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
-		if (typeof sampleActivity != 'number') {
+		if (t != t || t == Infinity) {
 			return false;
 		} else {
 			return t;
